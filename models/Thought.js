0b54a54b8bbf78d5529aefa671fbe2a649e887be
Fox <johnafoxwell@gmail.com>
2023-07-31 07:42:03 -0400

    Added models

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const dayjs = require("dayjs");
+const reactionSchema = require("./Reaction")
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: getDate,
+        },
+        username:{
+            type: String,
+            required: true,
+            ref: "user"
+        },
+        reactionBody: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+function getDate(date) {
+    return dayjs(date).format("MMM/DD/YYYY")
+};
+
+thoughtSchema
+    .virtual("reactionCount")
+    .get(function() {
+        return this.reactions.length;
+    })
+
+const Thought = model("thought", thoughtSchema)
+
+module.exports = Thought;
