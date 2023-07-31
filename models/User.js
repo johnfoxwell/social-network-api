6b2bbd4cff78688706bfcd713d9ab43d86bdd05f
Fox <johnafoxwell@gmail.com>
2023-07-31 07:18:30 -0400

    Added User model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, "Please enter a valid email address"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought"
+            }
+        ],
+        friends: [this],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+    .virtual("friendCount")
+    .get(function () {
+        return this.friends.length
+    });
+
+const User = model("user", userSchema);
+
+module.exports = User
